823e2a2d56255b3e436ac0201c77893c54a7684c
Mike Marino <mmarino@gmail.com>
2015-04-14 04:22:41 +0200

    First step towards exporting an interface.

--- Cascade/WrapDAQBox.cc
+++ Cascade/WrapDAQBox.cc
@@ -1,4 +1,4 @@
-#include "BuildLambdaFunction.hh" 
+#include "BuildLambdaFunction.hh"
 #include "WrapFirmware.hh"
 #include "WrapBoard.hh"
 #include "universe_api.h"
@@ -19,12 +19,9 @@ namespace cascade {
 
 }
 
-namespace {
-
 
 struct MeasurementData {
     std::string   firmwareVersion;
-    ULONGLONG     absTimeElapsed;
     ULONGLONG     lifeTimeElapsed;
     ULONGLONG     eventsCounted;
     ULONGLONG     sweepsElapsed;
@@ -32,9 +29,25 @@ struct MeasurementData {
     CFConfig      firmwareConfig;
     HSetup        hardwareConfig;
     MConfig       measurementConfig;
+    CFSimulation  simulationConfig;
     CFStatistics  statistics;
     CFDiagnostics diagnostics;
 };
+
+BOOST_FUSION_ADAPT_STRUCT(
+  MeasurementData,
+   (std::string  , firmwareVersion)
+   (ULONGLONG    , lifeTimeElapsed)
+   (ULONGLONG    , eventsCounted)
+   (ULONGLONG    , sweepsElapsed)
+   (double       , chopperFrequency)
+   (CFConfig     , firmwareConfig)
+   (HSetup       , hardwareConfig)
+   (MConfig      , measurementConfig)
+   (CFSimulation , simulationConfig)
+   (CFStatistics , statistics)
+   (CFDiagnostics, diagnostics)
+)
 
 typedef std::vector<DWORD> vec_type;
 
@@ -44,19 +57,53 @@ struct BinSizes {
     DWORD y;
 };
 
+BOOST_FUSION_ADAPT_STRUCT(
+  BinSizes,
+   (DWORD, time)
+   (DWORD, x)
+   (DWORD, y)
+)
+
 struct CurrentStatus {
     vec_type  data;
     BinSizes  bins;
     bool      isMeasuring;
     bool      isLocked;
+    ULONGLONG absTimeElapsed;
+    void Reset() { data.resize(bins.x*bins.y*bins.time); absTimeElapsed = 0;}
 };
 
-// Steps of measurement
-// 1. Configure Firmware
-// 2. Configure Measurement
-// 3. Configure simulation (if it is a simulation)
-// 4. Generate buffer, read out 
-// At the end, readout data
+BOOST_FUSION_ADAPT_STRUCT(
+  CurrentStatus,
+   (vec_type , data)
+   (BinSizes , bins)
+   (bool     , isMeasuring)
+   (bool     , isLocked)
+   (ULONGLONG, absTimeElapsed)
+)
+
+namespace {
+
+using cascade::detail::anyvec;
+using cascade::detail::anymap;
+
+typedef std::map<std::string, boost::any> check_return_map;
+check_return_map gReturnMap;
+
+template<typename T>
+void RegisterTypeCheck(std::function<T(const T&)> func)
+{
+  gReturnMap[typeid(T).name()] = func;
+}
+
+template<typename T>
+T PerformCheck(const T& val)
+{
+  auto check = gReturnMap.find(typeid(T).name());
+  if (check == gReturnMap.end()) return val;
+  return boost::any_cast<std::function<T(const T&)> >(check->second)(val);
+}
+
 
 TUniverseCDAQBox gDAQBox;
 
@@ -69,10 +116,10 @@ template<class classType, typename funcType, typename F, typename... Args>
 struct WrapFunction
 {
   typedef std::function<F(Args...)> return_type;
-  static return_type Wrap(classType& anobj, funcType func) 
+  static return_type Wrap(classType& anobj, funcType func)
   {
     return [&anobj,func] (Args... args) {
-      return (anobj.*func)(args...);
+      return PerformCheck((anobj.*func)(args...));
     };
   }
 };
@@ -100,36 +147,89 @@ class InitializeDevice {
   public:
   InitializeDevice()
   {
-    set_hw_byte_swap(true); 
+    set_hw_byte_swap(true);
     // get version of the used HardwareLib and for wich OS it is compiled for
     std::cout << gDAQBox.GetVersionHardwareLib() << std::endl;
     std::cout << gDAQBox.GetVersionOS() << std::endl;
-    
+
     // init the USB interface of the DAQBox
-    DWORD error;
-    std::cout << "0x" << std::hex << ( error = gDAQBox.Init( 0x400000 ) ) 
+    std::cout << "0x" << std::hex << ( PerformCheck(gDAQBox.Init( 0x400000 )) )
               << " : DAQBox Init" << std::endl;
+    gMeasurementData.firmwareVersion = gDAQBox.GetFirmwareVersion();
   }
 };
 
-InitializeDevice gDev;  
+InitializeDevice gDev;
+
+// Steps of measurement
+// 1. Configure Firmware
+// 2. Configure Measurement
+// 3. Configure simulation (if it is a simulation)
+// 4. Generate buffer, read out
+// At the end, readout data
+void StartMeasurement()
+{
+  // Check if we're running
+  if (gStatus.isLocked || gStatus.isMeasuring) {
+    throw cascade::daqbox_error("Card locked or running");
+  }
+  gStatus.isMeasuring = true;
+
+  // Grab the current setup
+  gMeasurementData.firmwareConfig = gDAQBox.GetActualConfigurationOfFirmware();
+  gMeasurementData.hardwareConfig = gDAQBox.GetActualHardwareSetup();
+  gMeasurementData.measurementConfig = gDAQBox.GetActualConfigurationOfMeasurement();
+  gMeasurementData.simulationConfig = gDAQBox.GetActualConfigurationOfFirmwareSimulation();
+
+  // Allocate the correct amount of data
+  gStatus.bins.time = gMeasurementData.measurementConfig.dwNoOfTimeBins;
+  gStatus.bins.x = gMeasurementData.measurementConfig.dwXResolution;
+  gStatus.bins.y = gMeasurementData.measurementConfig.dwYResolution;
+  gStatus.Reset();
+
+  PerformCheck(gDAQBox.InitDataBuffer(&gStatus.data[0]));
+  PerformCheck(gDAQBox.StartMeasurement());
 
 }
-/*
-                  any t = cascade::EncAll<CFConfig>::encode(CFConfig());
-                  cerr << t.type().name() << endl;
-                  session.call("send_to_database", {t}) 
-                  .then([&](future<any> f) {
-                     try {
-                        bool result = any_cast<bool> (f.get());
-                        cerr << "s2d() called with result: " << result << endl;
-                     } catch (...) {
-                        cerr << "s2d() call failed" << endl;
-                     }
-                  }
-                        );
 
-*/
+anymap GetCurrentStatus()
+{
+  PerformCheck(gDAQBox.Measurement());
+  gStatus.absTimeElapsed = gDAQBox.GetAbsTimeElapsed();
+  DWORD status;
+  PerformCheck(gDAQBox.GetStatusRegister(&status));
+  gStatus.isMeasuring = ((status & 0x8) == 0);
+  return cascade::EncAll<CurrentStatus>::encode(gStatus);
+}
+
+anymap StopMeasurement()
+{
+  // Check if we're running
+  if (!gStatus.isLocked) {
+    throw cascade::daqbox_error("Card should be locked when calling stop measurement");
+  }
+
+  PerformCheck(gDAQBox.StopMeasurement());
+  // Grab the current setup
+
+  bool stopped = false;
+  while(!stopped) {
+    PerformCheck(gDAQBox.HasMeasurementStopped(&stopped));
+  }
+
+  anymap config = cascade::EncAll<MeasurementData>::encode(gMeasurementData);
+  if (!gMeasurementData.simulationConfig.bSimulationCIPix) {
+    // Remove from the config
+    config.erase("simulationConfig");
+  }
+  config["status"] = GetCurrentStatus();
+
+  gStatus.isLocked = false;
+  return config;
+}
+
+
+}
 
 // Interface
 // Set/Get configuration
@@ -139,6 +239,13 @@ InitializeDevice gDev;
 namespace cascade {
 void define_daqbox_interface(cascade::session_type& s)
 {
+
+  RegisterTypeCheck<DWORD>([] (const DWORD& at) {
+    if (at != EC_OK) {
+      throw cascade::daqbox_error(gDAQBox.GetErrorText(at));
+    }
+    return at;
+  });
   #define NOWRAP(func, args...) \
   auto r1_ ## func = s.provide(#func, cascade::GenFunction(Wrap(gDAQBox, &TUniverseCDAQBox::func), ##args)); \
     r1_ ## func.then([](boost::future<autobahn::registration> reg) {\
@@ -147,8 +254,8 @@ void define_daqbox_interface(cascade::session_type& s)
 
    //NOWRAP(GetFirmwareVersReg)
    //NOWRAP(GetSerialNumber)
-   //.def<BL::strMemFn>("GetFirmwareVersion", &BL::GetFirmwareVersion ) 
-   //.def<BL::strMemFn>("GetFirmwareSetup", &BL::GetFirmwareVersion ) 
+   //.def<BL::strMemFn>("GetFirmwareVersion", &BL::GetFirmwareVersion )
+   //.def<BL::strMemFn>("GetFirmwareSetup", &BL::GetFirmwareVersion )
    //NOWRAP(GetFirmwareSetupReg)
    //    // setup of the CFBoard hardware
    NOWRAP(GetActualHardwareSetup)
