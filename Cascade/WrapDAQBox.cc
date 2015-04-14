7a0341ec9c27abadec3ec3f27eb2adbedbd2c0d8
Mike Marino <mmarino@gmail.com>
2015-04-14 11:15:26 +0200

    First try to run card.

--- Cascade/WrapDAQBox.cc
+++ Cascade/WrapDAQBox.cc
@@ -156,6 +156,98 @@ class InitializeDevice {
     std::cout << "0x" << std::hex << ( PerformCheck(gDAQBox.Init( 0x400000 )) )
               << " : DAQBox Init" << std::endl;
     gMeasurementData.firmwareVersion = gDAQBox.GetFirmwareVersion();
+    using std::cout;
+    using std::endl;
+    using std::hex;
+    // reset DAQBox, if wanted or needed
+    cout << "0x" << std::hex << gDAQBox.Reset() << " : DAQBox Reset" << endl;
+
+    // get firmware version of DAQBox
+    cout << gDAQBox.GetFirmwareVersion() << endl;
+
+    // get status register
+    DWORD status;
+    cout << "0x" << std::hex << gDAQBox.GetStatusRegister(&status) << " : DAQBox get status" << endl;
+    cout << "0x" << std::hex << status << endl << endl;
+
+    TUniverseCDAQBox* p_cf = &gDAQBox;
+    TUniverseCDAQBox* p_m = &gDAQBox;
+
+    CFConfig cconfig = p_cf->GetActualConfigurationOfFirmware();
+
+    // Configuration register of the filter algorithm for event reconstruction in time and space.
+    // ConfigReg = AntiAll(5+1+(NO_OF_CIPIX/2) - 0) |
+    //              AntiCorner(2 - 0) | TakeNoGainBit |
+    //              TakeBrokenBit | GEMTimeBit | NoFilterBit | ResetBit
+    // Further details under section "Filter Entity Reference" of CASCADE firmware manuals.
+    cconfig.dwEventFilterConfig = 0x1040;
+    // type of list mode data: raw data (RAW) or filtered (FILTERED) data
+    cconfig.dwTypeOfListModeData = RAW;
+    // dis/enable Slave mode
+    cconfig.bSlaveMode = false;
+
+    /// Which INPUT( No = 0, 1, 2, ... ) will be connected to which internal signal 'nSignalIn'.
+    /// Further details under section "DigitalIO Entity Reference" of CASCADE firmware manuals.
+    for (int i = 0; i < 4; i++ )
+        cconfig.dwInputMultiplexer[i] = i;
+    /// Which OUTPUT( No = 0, 1, 2, ... ) will be connected to which internal Signal 'nSignalOut'.
+    /// Further details under section "DigitalIO Entity Reference" of CASCADE firmware manuals.
+    for (int i = 0; i < 4; i++ )
+        cconfig.dwOutputMultiplexer[i] = i;
+    /// Length of TOF output signals (Chopper, NewTimeBin) in terms of 25ns (1 - 2^24-1) generated
+    /// from firmware.
+    cconfig.dwLengthTOFOut = 1000;
+
+    cout << hex << p_cf->ConfigureFirmware( cconfig ) << " cf config" << endl;
+
+    MConfig config = p_m->GetActualConfigurationOfMeasurement();
+
+    // config of the measurement type
+    config.dwTypeOfReadout = TWO_D_CROSSED_STRIPES;
+    config.dwTypeOfMeasurement = IMAGE;
+//  config.dwTypeOfMeasurement = TOF;
+    config.dwXResolution = 128;
+    config.dwYResolution = 128;
+
+    // measurement time in terms of 100ns
+    config.ullTimeToCount = 1*10000000; // 1 sec
+
+    // prameters for TOF
+    // dwelltime in terms of 100ns
+    config.dwDwellTime = 10000; // 1 msec
+    config.dwNoOfTimeBins = 1;
+    config.bBreakMode = true;
+
+    cout << hex << p_m->ConfigureMeasurement( config ) << " m config" << endl;
+
+
+
+    //--------------------- Configuration of a Simulation -----------------------------------------
+
+    CFSimulation simulation;
+    simulation.bSimulationCIPix = true;
+    simulation.dwNoOfPauseCycles = 2;
+
+    // j = 0 (low-word CIPix 0), 1 (high-word CIPix 0), 2 (low-word CIPix 1), etc.
+    DWORD j = 0;
+    simulation.dwXSimulationPattern[0][j] = 0x00000001;
+    simulation.dwXSimulationPattern[1][j] = 0x00000000;
+    simulation.dwXSimulationPattern[2][j] = 0x80000000;
+    simulation.dwXSimulationPattern[3][j] = 0x00000008;
+    simulation.dwXSimulationPattern[4][j] = 0x00000000;
+    simulation.dwXSimulationPattern[5][j] = 0x00000000;
+    simulation.dwXSimulationPattern[6][j] = 0x00000008;
+    simulation.dwXSimulationPattern[7][j] = 0x00000008;
+
+    simulation.dwYSimulationPattern[0][j] = 0x00000001;
+    simulation.dwYSimulationPattern[1][j] = 0x00000000;
+    simulation.dwYSimulationPattern[2][j] = 0x80000000;
+    //...
+
+    simulation.bSimulationChopper = false;
+    simulation.dwChopperPeriod = 1000000; // 100ms
+
+    cout << hex << p_cf->ConfigureFirmwareSimulation( simulation ) << " cf sim" << endl;
   }
 };
 
@@ -167,7 +259,7 @@ InitializeDevice gDev;
 // 3. Configure simulation (if it is a simulation)
 // 4. Generate buffer, read out
 // At the end, readout data
-void StartMeasurement()
+boost::any StartMeasurement()
 {
   // Check if we're running
   if (gStatus.isLocked || gStatus.isMeasuring) {
@@ -189,10 +281,10 @@ void StartMeasurement()
 
   PerformCheck(gDAQBox.InitDataBuffer(&gStatus.data[0]));
   PerformCheck(gDAQBox.StartMeasurement());
-
+  return true;
 }
 
-anymap GetCurrentStatus()
+boost::any GetCurrentStatus()
 {
   PerformCheck(gDAQBox.Measurement());
   gStatus.absTimeElapsed = gDAQBox.GetAbsTimeElapsed();
@@ -202,7 +294,7 @@ anymap GetCurrentStatus()
   return cascade::EncAll<CurrentStatus>::encode(gStatus);
 }
 
-anymap StopMeasurement()
+boost::any StopMeasurement()
 {
   // Check if we're running
   if (!gStatus.isLocked) {
@@ -217,7 +309,15 @@ anymap StopMeasurement()
     PerformCheck(gDAQBox.HasMeasurementStopped(&stopped));
   }
 
-  anymap config = cascade::EncAll<MeasurementData>::encode(gMeasurementData);
+  PerformCheck(gDAQBox.ReadStatistics(&gMeasurementData.statistics));
+  PerformCheck(gDAQBox.ReadDiagnostics(&gMeasurementData.diagnostics));
+
+  gMeasurementData.lifeTimeElapsed = gDAQBox.GetLifeTimeElapsed();
+  gMeasurementData.eventsCounted = gDAQBox.GetEventsCounted();
+  gMeasurementData.sweepsElapsed = gDAQBox.GetSweepsElapsed();
+  gMeasurementData.chopperFrequency = gDAQBox.GetChopperFrequency();
+
+  anymap config = boost::any_cast<anymap>(cascade::EncAll<MeasurementData>::encode(gMeasurementData));
   if (!gMeasurementData.simulationConfig.bSimulationCIPix) {
     // Remove from the config
     config.erase("simulationConfig");
@@ -246,8 +346,14 @@ void define_daqbox_interface(cascade::session_type& s)
     }
     return at;
   });
-  #define NOWRAP(func, args...) \
-  auto r1_ ## func = s.provide(#func, cascade::GenFunction(Wrap(gDAQBox, &TUniverseCDAQBox::func), ##args)); \
+  #define NOWRAP(func) \
+  auto r1_ ## func = s.provide(#func, cascade::GenFunction(Wrap(gDAQBox, &TUniverseCDAQBox::func))); \
+    r1_ ## func.then([](boost::future<autobahn::registration> reg) {\
+       std::cerr << "procedure " #func "() registered with registration ID " << reg.get().id << std::endl;\
+    }).wait();
+
+  #define NOOBJWRAP(func) \
+  auto r1_ ## func = s.provide(#func, cascade::GenFunction(&func)); \
     r1_ ## func.then([](boost::future<autobahn::registration> reg) {\
        std::cerr << "procedure " #func "() registered with registration ID " << reg.get().id << std::endl;\
     }).wait();
@@ -270,6 +376,9 @@ void define_daqbox_interface(cascade::session_type& s)
    NOWRAP(GetActualConfigurationOfMeasurement)
    NOWRAP(ConfigureMeasurement)
 
+   NOOBJWRAP(StartMeasurement)
+   NOOBJWRAP(StopMeasurement)
+   NOOBJWRAP(GetCurrentStatus)
    //    //virtual DWORD InitDataBuffer( DWORD *i_pDataBuffer );
    //    //DWORD SetDataBuffer( DWORD *i_pDataBuffer );
 
@@ -278,23 +387,14 @@ void define_daqbox_interface(cascade::session_type& s)
 
    //    //std::string GetStatusErrorText( const DWORD ErrorCode );
    //    // start, stop and measurement
-   NOWRAP(StartMeasurement)
-   NOWRAP(Measurement)
+   //NOWRAP(StartMeasurement)
+   //NOWRAP(Measurement)
    //NOWRAP(GetListModeData)
-   NOWRAP(StopMeasurement)
-   NOWRAP(StopTOFMeasurementImmediately)
+   //NOWRAP(StopMeasurement)
+   //NOWRAP(StopTOFMeasurementImmediately)
    //NOWRAP(HasMeasurementStopped)
-   NOWRAP(GetEntireMeasurement)
+   //NOWRAP(GetEntireMeasurement)
    //    // actual status of measurement
-   //NOWRAP(ReadStatistics)
-   //NOWRAP(ReadDiagnostics)
-   NOWRAP(GetExternalCounter)
-   // get measured data
-   NOWRAP(GetAbsTimeElapsed)
-   NOWRAP(GetLifeTimeElapsed)
-   NOWRAP(GetEventsCounted)
-   NOWRAP(GetSweepsElapsed)
-   NOWRAP(GetChopperFrequency)
 
    //    // read from Event Data FIFO
    //    //DWORD BlockReadEDFifo( DWORD *pData, DWORD ReqNoOfDWords );
